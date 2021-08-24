6621c7d62f068f51da212f0c121db8c58b4d71cb
melnik <Boherave21>
2021-08-24 23:08:49 +0300

    Members section. Add load members from json

--- #src/js/files/script.js
+++ #src/js/files/script.js
@@ -2,10 +2,13 @@ document.addEventListener("DOMContentLoaded", ready);
 
 function ready() {
     document.addEventListener('click', documentActions);
-    function documentActions(e) {
+    async function documentActions(e) {
         const target = e.target;
         if (target.classList.contains('item-member__expand')) {
-            showHideDescriptionHandler(target)
+            showHideDescriptionHandler(target);
+        }
+        if (target.classList.contains('members__show-more')) {
+            await loadOtherMembers(target);
         }
     }
 }
@@ -18,6 +21,25 @@ function showHideDescriptionHandler(target) {
     hideDescriptionNode.classList.toggle('show-hide');
 }
 
+async function loadOtherMembers(button) {
+    const file = 'data/members.json';
+    button.classList.add('_hold');
+    const data = await loadData(file);
+    let cards = [];
+    if(data && data.members) {
+        cards = data.members.map(renderMembersCard)
+    }
+    addMembersToDocument(cards);
+}
+
+function addMembersToDocument(membersCard) {
+    const membersList = document.querySelector('.members__list');
+    console.log(membersList)
+    membersCard.forEach(card => {
+        membersList.insertAdjacentHTML('beforeend', card);
+    })
+}
+
 async function loadData(file) {
     let response = await fetch(file, {
         method: 'GET'
@@ -27,4 +49,30 @@ async function loadData(file) {
     } else {
         alert("Ошибка");
     }
+}
+
+function renderMembersCard(member) {
+    const template =
+      `
+      <div class="members__item item-member">
+        <div class="item-member__body">
+          <div class="item-member__header">
+            <div class="item-member__name">${member.name}</div>
+            <div class="item-member__position"></div>
+          </div>
+          <div class="item-member__photo _ibg">
+            <img src="${member.photo}" alt="${member.altText}">
+          </div>
+          <div class="item-member__description description-member">
+            <div class="description-member__part">${member.description}</div>
+            <div class="description-member__part description-member__part_hide">${member.additionalDescription}</div>
+          </div>
+          <div class="item-member__footer">
+            <button  class="item-member__expand _icon-expand-arrow"></button>
+          </div>
+        </div>
+      </div>
+      `;
+
+    return template;
 }
